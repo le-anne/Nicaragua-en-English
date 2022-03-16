a214625245bf44f1dc72b556966edd7d7f6c7fc5
Leanne <76232496+le-anne@users.noreply.github.com>
2022-03-15 17:24:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const PORT = process.env.PORT || 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const app = express();
+
+const newspapers = [
+  {
+    name: "Google",
+    address: "https://thehill.com/opinion/international",
+    base: "",
+  },
+  {
+    name: "NCR",
+    address: "https://www.ncronline.org/news/people",
+    base: "",
+  },
+  {
+    name: "guardian",
+    address: "https://www.theguardian.com/world/nicaragua",
+    base: "",
+  },
+];
+
+const articles = [];
+
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("nicaragua")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+
+      articles.push({
+        title,
+        url: newspaper.base + url,
+        source: newspaper.name,
+      });
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json("Nicaragua News en English");
+});
+
+app.get("/news", (req, res) => {
+  res.json(articles);
+});
+
+app.get("/news/:newspaperId", (req, res) => {
+  const newspaperId = req.params.newspaperId;
+
+  const newspaperAddress = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].address;
+  const newspaperBase = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].base;
+
+  axios
+    .get(newspaperAddress)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const specificArticles = [];
+
+      $('a:contains("nicaragua")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr("href");
+        specificArticles.push({
+          title,
+          url: newspaperBase + url,
+          source: newspaperId,
+        });
+      });
+      res.json(specificArticles);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
