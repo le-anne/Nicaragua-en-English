a1bad884820fc82cc86d970493d8c90039ba1c63
Leanne <76232496+le-anne@users.noreply.github.com>
2023-03-30 19:38:43 -0700

    reverted to webpack4 lol

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,16 +1,10 @@
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  mode: "production",
-  resolve: {
-    fallback: {
-      zlib: require.resolve("browserify-zlib"),
-      querystring: require.resolve("querystring-es3"),
-    },
+    filename: "bundle.js",
   },
 };
